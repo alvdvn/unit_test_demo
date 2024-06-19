0baf9481b550231da36497a45e8ad79a376da5af
DongNH_DT96 <alvdvip@gmail.com>
2024-06-19 16:30:28 +0700

    update test file

--- test/demo_widget_test.dart
+++ test/demo_widget_test.dart
@@ -1,30 +1,82 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:unit_test_demo/main.dart';
+import 'package:get/get.dart';
+import 'package:unit_test_demo/screen/demo_widget.dart';
+import 'package:unit_test_demo/screen/demo_controller.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  setUp(() {
+    // Initialize Get for the tests
+    Get.testMode = true;
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  testWidgets('DemoWidget test', (WidgetTester tester) async {
+    // Mock DemoController for testing
+    final DemoController mockController = DemoController();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Build our widget and trigger a frame.
+    await tester.pumpWidget(
+      GetMaterialApp(
+        home: Scaffold(
+          body: DemoWidget(),
+        ),
+        // Provide the DemoController instance as a dependency
+        initialBinding: BindingsBuilder(() {
+          Get.lazyPut<DemoController>(() => mockController);
+        }),
+      ),
+    );
+
+    // Verify initial screen content (for example, check for a specific text)
+    expect(find.text('Home Screen Content'), findsOneWidget);
+
+    // Simulate tapping on the home icon
+    await tester.tap(find.byIcon(Icons.home));
+    await tester.pump(); // Rebuild the widget after the tap
+
+    // Verify content after changing to tab 0
+    expect(find.text('Item 0'), findsOneWidget);
+
+    await tester.tap(find.text('Item 0'));
+    await tester.pump();
+    // Verify if the SnackBar is shown
+    expect(find.text('Item 0'), findsNWidgets(2));
+    await tester.runAsync(() => Future.delayed(Duration(seconds: 2)));
+
+    // Simulate tapping on another icon (for example, library books icon)
+    await tester.tap(find.byIcon(Icons.library_books_rounded));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify content after changing to tab 1
+    expect(find.text('Library Screen Content'), findsOneWidget);
+    expect(find.text('Item 11'), findsNothing); // Ensure previous screen content is not visible
+
+    // Verify if controller state is updated correctly
+    expect(mockController.tabIndex.value, 1);
+
+    // Simulate tapping on contact mail icon
+    await tester.tap(find.byIcon(Icons.contact_mail_outlined));
+    await tester.pump();
+
+    // Verify content after changing to tab 2
+    expect(find.text('Contact Screen Content'), findsOneWidget);
+    expect(find.text('Library Screen Content'), findsNothing); // Ensure previous screen content is not visible
+
+    // Verify if controller state is updated correctly
+    expect(mockController.tabIndex.value, 2);
+
+    // Simulate tapping on person icon
+    await tester.tap(find.byIcon(Icons.person));
+    await tester.pump();
+
+    // Verify content after changing to tab 3
+    expect(find.text('Profile Screen Content'), findsOneWidget);
+    expect(find.text('Contact Screen Content'), findsNothing); // Ensure previous screen content is not visible
+
+    // Verify if controller state is updated correctly
+    expect(mockController.tabIndex.value, 3);
+
+    // Now let's test clicking on an item in the Home screen
+    // One in the list, one in the SnackBar
   });
 }
