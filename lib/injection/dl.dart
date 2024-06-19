5bdc05725869d20cd0dd2f5e363e5c54ba4e00c0
DongNH_DT96 <alvdvip@gmail.com>
2024-06-19 09:06:29 +0700

    put depentences

--- lib/injection/dl.dart
+++ lib/injection/dl.dart
@@ -1,5 +1,8 @@
 
 
-Future<void> configutionDepentences() async{
+import 'package:get/instance_manager.dart';
+import 'package:unit_test_demo/screen/demo_controller.dart';
 
+Future<void> configutionDepentences() async{
+Get.put<DemoController>;
 }
